8e779b3eee70c2f1aaa8857dd6db62c2f53a47c4
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-02-06 19:24:32 +1000

    Typos

--- test_neural_style.py
+++ test_neural_style.py
@@ -1,4 +1,5 @@
 from neural_style import NSTModel
 
-nsf = NSTModel(content_path='img/bridge.jpg', style_path='img/ab1.jpg', h=768, w=1024, style_weights=[0.5, 1, 2, 3, 4])
-nsf.run(num_iter=2000, beta=1)
+# Need to specify the content image path and the style image path
+nst = NSTModel(content_path='img/bridge.jpg', style_path='img/ab1.jpg', h=768, w=1024, style_weights=[0.5, 1, 2, 3, 4])
+nst.run(num_iter=2000, beta=1)
--- test_vgg.py
+++ test_vgg.py
@@ -4,6 +4,7 @@ from vgg_model import Model
 import numpy as np
 from utilities import predict
 
+# Load image, need to specify the path
 image = load_image('img/gr.jpg', shape=[224, 224], preprocess=True, bgr=False)
 model = Model('imagenet-vgg-verydeep-19.mat')
 out = model.build_model()
@@ -11,5 +12,6 @@ with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     sess.run(model.tf_layers['input'].assign(image))
     prob = sess.run(out)
+    # Need to read 'synset.txt' file to print out the name of the class
     pred, prob = predict(prob[0], './synset.txt')
     print(pred, prob)
