f93be14cc4acd777d506e247b2d5a335e01cb42f
Siyu Liu <siyu.liu2@uqconnect.edu.au>
2018-02-06 21:28:47 +1000

    Removed some redundancy

--- test_neural_style.py
+++ test_neural_style.py
@@ -1,6 +1,6 @@
 from neural_style import NSTModel
 
 # Need to specify the content image path and the style image path
-nst = NSTModel(content_path='img/bridge.jpg', style_path='img/p1.jpg', h=768, w=1024, style_weights=[0.5, 1, 2, 3, 4])
-nst.run(num_iter=2000, beta=1)
+nst = NSTModel(content_path='img/bridge.jpg', style_path='img/ab6.jpg', h=768, w=1024)
+nst.run(num_iter=2000)
 
